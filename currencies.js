99ac91e9505abad60b2eccccc76159ef182f3997
Joao <joaoricardotg@gmail.com>
2020-10-14 13:14:45 -0300

    Add files via upload

---
+++ currencies.js
@@ -0,0 +1,18 @@
+let currencies = {
+    USD:'$',
+    BRL:'R$',
+    CAD:'C$',
+    GBP:'£',
+    CHF:'Fr.',
+    AUD:'AU$',
+    NZD:'NZ$',
+    JPY:'¥',
+    SEK:'SEK',
+    CNY:'¥',
+    INR:'₹',
+    PHP:'₱',
+    RUB:'₽',
+    EUR:'€'
+}
+
+export {currencies}
--- index.js
+++ index.js
@@ -1,30 +1,47 @@
 let fetchResult
-let dollarValue;
+let currValue;
 let hourValue;
 let hoursWorkedPerDay;
 let daysWorkedPerMonth;
 let taxes;
 let calculationResult;
 let message;
+import { currencies } from './currencies.js';
+let secondSelectMustBeEqualtoFirstSelect = true;
 
-let getDollarValue = async () => {
-    console.log('Fetching dollar value')
+let getCurrValue = async () => {
+    console.log('Fetching currency value')
     fetchResult = await fetch('https://api.exchangeratesapi.io/latest?base=USD');
-    dollarValue = await fetchResult.json();
-    console.log(dollarValue.rates.BRL);
+    currValue = await fetchResult.json();
+    console.log(currValue.rates[document.getElementsByClassName('firstSelect')[0].value]/currValue.rates[document.getElementsByClassName('secondSelect')[0].value]);
+    currValue = currValue.rates[document.getElementsByClassName('secondSelect')[0].value]/currValue.rates[document.getElementsByClassName('firstSelect')[0].value];
+}
+
+let setSecondSelect = () => {
+    if (secondSelectMustBeEqualtoFirstSelect) {
+        console.log(document.getElementsByClassName('firstSelect')[0].value)
+        document.getElementsByClassName('secondSelect')[0].value = document.getElementsByClassName('firstSelect')[0].value;
+    }
 }
 
 let calc = async () => {
     console.log('Calculating...')
-    await getDollarValue();
+    await getCurrValue();
     hourValue = document.getElementsByClassName('hourValue')[0].value;
     hoursWorkedPerDay = document.getElementsByClassName('numberHoursPerDay')[0].value;
     daysWorkedPerMonth = document.getElementsByClassName('numberDaysPerMonth')[0].value;
     taxes = document.getElementsByClassName('taxes')[0].value;
-    calculationResult = hourValue * hoursWorkedPerDay * daysWorkedPerMonth * ((100 - taxes)/100) * dollarValue.rates.BRL;
+    calculationResult = hourValue * hoursWorkedPerDay * daysWorkedPerMonth * ((100 - taxes) / 100) * currValue;
     message = document.getElementsByClassName('message');
-    console.log([hourValue, hoursWorkedPerDay, daysWorkedPerMonth, taxes, dollarValue]);
-    message[0].innerHTML = 'R$ ' +calculationResult.toFixed(2);
+    console.log([hourValue, hoursWorkedPerDay, daysWorkedPerMonth, taxes, currValue]);
+    message[0].innerHTML = currencies[document.getElementsByClassName('secondSelect')[0].value] + ' ' + calculationResult.toFixed(2);
+    message[0].className = 'message';
+    document.getElementsByClassName('secondSelect')[0].className = 'secondSelect';
+    document.getElementsByClassName('switchCurr')[0].className = 'small switchCurr';
+    secondSelectMustBeEqualtoFirstSelect = false;
 }
 
-let button = document.getElementsByClassName('btn')[0].addEventListener('click', calc);
+
+document.getElementsByClassName('firstSelect')[0].onchange = setSecondSelect;
+document.getElementsByClassName('btn')[0].addEventListener('click', calc);
+document.getElementsByClassName('secondSelect')[0].onchange = calc;
